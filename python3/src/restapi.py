85e8a4a0b0e0613cb5af8d79d74c86e62613f4e7
Edwin M <edwingmundo2@gmail.com>
2023-12-12 20:23:21 -0500

    adding some basics of jwt and protected routes
    Yes I know the super duper secret key is in the code. This will be an env var when I ship this api.

--- python3/src/restapi.py
+++ python3/src/restapi.py
@@ -1,27 +1,86 @@
-from fastapi import FastAPI, Depends, HTTPException, status, Response, Request
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from fastapi.responses import JSONResponse
-from datetime import datetime, timedelta
+#!../bin/python
+import sqlite3
+from fastapi import FastAPI, Form, HTTPException, Response, Cookie, Depends
+from fastapi.security import OAuth2PasswordBearer
 import jwt
+from datetime import datetime, timedelta, timezone
+
 
 app = FastAPI()
-
-# Secret key to sign and verify the JWT
+# Secret key to sign JWT tokens
 SECRET_KEY = "your-secret-key"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-REFRESH_TOKEN_EXPIRE_MINUTES = 30
-
-user_database = {
-    "username": "eddy",
-    "password": "123",
-}
-
-# OAuth2PasswordBearer for access token
+# Token expiration time in minutes
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def create_jwt_token(data: dict, expires_delta: timedelta):
+class Sqlite_db():
+    def database_create_user_table(self):
+        with sqlite3.connect('trading_database.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY,
+                        username TEXT UNIQUE,
+                        password TEXT
+                    )''')
+
+    def database_create_user(self, username: str, password: str):
+        with sqlite3.connect('trading_database.db') as connection:
+            cursor = connection.cursor()
+            try:
+                cursor.execute(
+                    'INSERT INTO users (username, password) VALUES (?, ?)',
+                    (username, password))
+            except sqlite3.IntegrityError:
+                print("Warn: The user alredy exists in the database")
+
+    def database_select_user_by_username(self, username: str) -> tuple | None:
+        with sqlite3.connect('trading_database.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                'SELECT * FROM users WHERE username = ?',
+                (username,))
+            row = cursor.fetchone()
+            return row
+
+    def database_create_session_table(self):
+        with sqlite3.connect('trading_database.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS sessions (
+                session_id INTEGER PRIMARY KEY,
+                user_id INTEGER,
+                jwt_token TEXT NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users(user_id));''')
+
+    def create_session(self, user_id: int, session_token: str):
+        with sqlite3.connect('trading_database.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute('''INSERT INTO sessions
+                (user_id, jwt_token)
+                VALUES (?, ?)''', (user_id, session_token))
+
+    def get_session_by_jwt(self, session_token: str):
+        with sqlite3.connect('trading_database.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                '''SELECT * FROM sessions WHERE jwt_token = (?)''',
+                (session_token,))
+            row = cursor.fetchone()
+            print(row)
+
+
+def validate_user(user_record: tuple, username: str, password: str) -> bool:
+    if username != user_record[1]:
+        return False
+    if password != user_record[2]:
+        return False
+    return True
+
+
+# Function to create access token
+def create_access_token(data: dict, expires_delta: timedelta):
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
@@ -29,101 +88,57 @@ def create_jwt_token(data: dict, expires_delta: timedelta):
     return encoded_jwt
 
 
-def confirm_database_user(username: str, password: str):
-    if (username != user_database['username']
-            or password != user_database['password']):
-        return False
-    return True
-
-
-def decode_refresh_token(token: str):
-    try:
-        jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return True
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token has expired",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    except jwt.InvalidTokenError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-
-def decode_auth_token(token: str, reload_token: str):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
-    except jwt.ExpiredSignatureError:
-        if decode_refresh_token(reload_token):
-            token = create_jwt_token(
-                {"sub": "123"},
-                timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-            return token
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token has expired",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    except jwt.InvalidTokenError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-
-@app.get('/')
-def home():
-    return JSONResponse("Hello")
-
-
-@app.post('/login')
-def token(
-    response: Response,
-    form_data: OAuth2PasswordRequestForm = Depends()
-):
-    username = form_data.username
-    password = form_data.password
-
-    if not confirm_database_user(username, password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},)
-
-    access_token = create_jwt_token(
-        {"sub": username},
-        timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+# Function to decode and verify the JWT token
+def get_current_user(session_token: str = Cookie(None, alias='access_token')):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
 
-    refresh_token = create_jwt_token(
-        {"sub": username},
-        timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
-    )
-    response.set_cookie(
-        key="JWT-Refresh",
-        value=refresh_token,
-        httponly=True,
-        secure=True
-    )
+    # Try to use the token from the cookie if not provided in the header
+    if session_token:
+        try:
+            payload = jwt.decode(
+                session_token,
+                SECRET_KEY,
+                algorithms=[ALGORITHM])
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=401, detail="Token has expired")
+        except jwt.InvalidTokenError:
+            raise credentials_exception
+    else:
+        raise credentials_exception
 
-    return {"access_token": access_token}
+
+@app.post("/login")
+async def login(
+        response: Response,
+        username: str = Form(),
+        password: str = Form()):
+    db = Sqlite_db()
+    user_record = db.database_select_user_by_username(username)
+    if user_record and validate_user(user_record, username, password):
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        cookie_expires = datetime.utcnow() + access_token_expires
+        cookie_expires_utc = cookie_expires.replace(tzinfo=timezone.utc)
+        access_token = create_access_token(
+            data={"sub": username},
+            expires_delta=access_token_expires)
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            expires=cookie_expires_utc,
+            httponly=True,
+            secure=True)
+        return "User has been logged in"
+    raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
-@app.get("/protected")
-def protected_route(
-    request: Request,
-    auth_token: str = Depends(oauth2_scheme),
-):
-    reload_token = request.cookies.get("JWT-Refresh")
-    if reload_token:
-        decode_auth_token(auth_token, reload_token)
-        return {"message": "This is a protected route"}
+@app.get("/protected-route")
+async def protected_route(current_user: dict = Depends(get_current_user)):
+    return {"message": "This is a protected route", "user": current_user}
 
 
 if __name__ == "__main__":
