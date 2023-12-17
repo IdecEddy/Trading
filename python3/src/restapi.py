79adcdcdd489520d1929f3483b521a8ae33665bd
Edwin M <edwingmundo2@gmail.com>
2023-12-17 01:46:55 -0500

    Adding test for the api and some logging.
    This adds some test and logging for the restapi. I hope this wont brake anything

--- python3/src/restapi.py
+++ python3/src/restapi.py
@@ -1,17 +1,38 @@
 #!../bin/python
 import sqlite3
+import logging
+from logging.handlers import RotatingFileHandler
+from ssl import cert_time_to_seconds
 from fastapi import FastAPI, Form, HTTPException, Response, Cookie, Depends
 from fastapi.security import OAuth2PasswordBearer
 import jwt
 from datetime import datetime, timedelta, timezone
+import os
 
+python_path = os.environ.get("PYTHONPATH")
+log_file_path = f"{python_path}logs/trading.log"
+file_format = "%(asctime)s - %(levelname)s - %(message)s"
+stdout_format = "%(levelname)s:     %(message)s"
+# Create handlers
+file_handler = RotatingFileHandler(log_file_path, maxBytes=10000, backupCount=3)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter(file_format))
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(logging.Formatter(stdout_format))
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 app = FastAPI()
 # Secret key to sign JWT tokens
 SECRET_KEY = "your-secret-key"
 ALGORITHM = "HS256"
 # Token expiration time in minutes
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 20
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -88,28 +109,48 @@ def create_access_token(data: dict, expires_delta: timedelta):
     return encoded_jwt
 
 
-# Function to decode and verify the JWT token
-def get_current_user(session_token: str = Cookie(None, alias='access_token')):
-    credentials_exception = HTTPException(
+def auth_user(session_token: str = Cookie(None, alias='access_token')):
+    credentials_exceptions = HTTPException(
         status_code=401,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+        detail="Could not validate credentials.",
+        headers={"WWW-Authenticate": "Bearer"})
+    credentials_expired_exceptions = HTTPException(
+        status_code=401,
+        detail="Token has expired.",
+        headers={"WWW-Authenticate": "Bearer"})
+    credentials_audience_exceptions = HTTPException(
+        status_code=401,
+        detail="Token was provided to a invalid audience.",
+        headers={"WWW-Authenticate": "Bearer"})
+    credentials_issuer_exceptions = HTTPException(
+        status_code=401,
+        detail="Token is from a invalid issuer.",
+        headers={"WWW-Authenticate": "Bearer"})
 
-    # Try to use the token from the cookie if not provided in the header
-    if session_token:
-        try:
-            payload = jwt.decode(
-                session_token,
-                SECRET_KEY,
-                algorithms=[ALGORITHM])
-            return payload
-        except jwt.ExpiredSignatureError:
-            raise HTTPException(status_code=401, detail="Token has expired")
-        except jwt.InvalidTokenError:
-            raise credentials_exception
-    else:
-        raise credentials_exception
+    if not session_token:
+        logger.info("auth_user: No session token was provided by requester.")
+        raise credentials_exceptions
+    try:
+        payload = jwt.decode(
+            session_token,
+            SECRET_KEY,
+            audience="tradingapi",
+            issuer="tradingapi",
+            algorithms=[ALGORITHM])
+        logger.info("auth_user: The token provided was a valid token")
+        return payload
+    except jwt.ExpiredSignatureError:
+        logger.warning("auth_user: The session token provided by the requester has expired.")
+        raise credentials_expired_exceptions
+    except jwt.InvalidAudienceError:
+        logger.warning("auth_user: The session token was provided to a invalid audience.")
+        raise credentials_audience_exceptions
+    except jwt.InvalidIssuerError:
+        logger.warning("auth_user: The session token is from a invalid issuer.")
+        raise credentials_issuer_exceptions
+    except jwt.InvalidTokenError:
+        logger.warning("auth_user: The session token provided by the requester is not valid.")
+        raise credentials_exceptions
 
 
 @app.get("/")
@@ -129,12 +170,14 @@ async def login(
         cookie_expires = datetime.utcnow() + access_token_expires
         cookie_expires_utc = cookie_expires.replace(tzinfo=timezone.utc)
         access_token = create_access_token(
-            data={"sub": username},
+            data={
+                "sub": username,
+                "iss": "tradingapi",
+                "aud": "tradingapi"},
             expires_delta=access_token_expires)
         response.set_cookie(
             key="access_token",
             value=access_token,
-            expires=cookie_expires_utc,
             httponly=True,
             secure=True)
         return "User has been logged in"
@@ -142,7 +185,7 @@ async def login(
 
 
 @app.get("/protected-route")
-async def protected_route(current_user: dict = Depends(get_current_user)):
+async def protected_route(current_user: dict = Depends(auth_user)):
     return {"message": "This is a protected route", "user": current_user}
 
 
--- python3/tests/test_restAPI.py
+++ python3/tests/test_restAPI.py
@@ -1,19 +1,7 @@
 from fastapi.testclient import TestClient
 from src.restapi import Sqlite_db, app, SECRET_KEY, ALGORITHM
+from datetime import timedelta, datetime
 import jwt
-
-
-# Mocking the database for testing purposes
-class MockSqliteDatabase(Sqlite_db):
-    def __init__(self):
-        pass
-
-    def database_select_user_by_username(self, username):
-        # Customize the mock database response based on your test cases
-        if username == "existing_user":
-            return (1, "existing_user", "hashed_password")
-        else:
-            return None
 
 
 client = TestClient(app)
@@ -21,29 +9,146 @@ client = TestClient(app)
 
 def test_protected_route_with_valid_token():
     # Generate a valid token for testing
-    valid_token = jwt.encode({"sub": "testuser"}, SECRET_KEY, algorithm=ALGORITHM)
+    valid_token = jwt.encode(
+            {
+                "sub": "testuser",
+                "iss": "tradingapi",
+                "aud": "tradingapi"
+            },
+            SECRET_KEY,
+            algorithm=ALGORITHM)
 
     # Make a request with the valid token
-    response = client.get("/protected-route", cookies={"access_token": valid_token})
+    response = client.get(
+            "/protected-route",
+            cookies={"access_token": valid_token})
 
     # Assert that the response status code is 200
     assert response.status_code == 200
 
-    # Assert that the response contains the expected message and user information
-    assert response.json() == {"message": "This is a protected route", "user": {"sub": "testuser"}}
+    # Assert that the response
+    # contains the expected message and user information
+    assert response.json() == {
+            "message": "This is a protected route",
+            "user": {
+                "sub": "testuser",
+                "iss": "tradingapi",
+                "aud": "tradingapi",
+            }}
 
 
-def test_protected_route_with_invalid_token():
+def test_protected_route_with_expired_token():
+    # Generate a valid token for testing
+    access_token_expires = timedelta(minutes=-5)
+    expire = datetime.utcnow() + access_token_expires 
+    valid_token = jwt.encode(
+            {
+                "sub": "testuser",
+                "iss": "tradingapi",
+                "aud": "tradingapi",
+                "exp": expire
+            },
+            SECRET_KEY,
+            algorithm=ALGORITHM)
+
+    # Make a request with the valid token
+    response = client.get(
+            "/protected-route",
+            cookies={"access_token": valid_token})
+
+    # Assert that the response status code is 200
+    assert response.status_code == 401
+
+    # Assert that the response
+    # contains the expected message and user information
+    assert response.json() == {
+        "detail": "Token has expired.",
+    }
+
+
+def test_protected_route_with_invalid_iss():
+    # Generate a valid token for testing
+    access_token_expires = timedelta(minutes=5)
+    expire = datetime.utcnow() + access_token_expires 
+    valid_token = jwt.encode(
+            {
+                "sub": "testuser",
+                "iss": "evil_iss",
+                "aud": "tradingapi",
+                "exp": expire
+            },
+            SECRET_KEY,
+            algorithm=ALGORITHM)
+
+    # Make a request with the valid token
+    response = client.get(
+            "/protected-route",
+            cookies={"access_token": valid_token})
+
+    # Assert that the response status code is 200
+    assert response.status_code == 401
+
+    # Assert that the response
+    # contains the expected message and user information
+    assert response.json() == {
+        "detail": "Token is from a invalid issuer.",
+    }
+
+
+def test_protected_route_with_invalid_aud():
+    # Generate a valid token for testing
+    access_token_expires = timedelta(minutes=5)
+    expire = datetime.utcnow() + access_token_expires 
+    valid_token = jwt.encode(
+            {
+                "sub": "testuser",
+                "iss": "tradingapi",
+                "aud": "evil_aud",
+                "exp": expire
+            },
+            SECRET_KEY,
+            algorithm=ALGORITHM)
+
+    # Make a request with the valid token
+    response = client.get(
+            "/protected-route",
+            cookies={"access_token": valid_token})
+
+    # Assert that the response status code is 200
+    assert response.status_code == 401
+
+    # Assert that the response
+    # contains the expected message and user information
+    assert response.json() == {
+        "detail": "Token was provided to a invalid audience.",
+    }
+
+
+def test_protected_route_with_no_token():
     # Make a request with an invalid token
-    response = client.get("/protected-route", cookies={"access_token": "invalid_token"})
+    response = client.get(
+            "/protected-route") 
 
     # Assert that the response status code is 401
     assert response.status_code == 401
 
     # Assert that the response contains the expected error message
-    assert response.json() == {"detail": "Could not validate credentials"}
+    assert response.json() == {"detail": "Could not validate credentials."}
 
-    
+
+def test_protected_route_with_invalid_token():
+    # Make a request with an invalid token
+    response = client.get(
+            "/protected-route",
+            cookies={"access_token": "invalid_token"}) 
+
+    # Assert that the response status code is 401
+    assert response.status_code == 401
+
+    # Assert that the response contains the expected error message
+    assert response.json() == {"detail": "Could not validate credentials."}
+
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
@@ -111,4 +216,3 @@ def test_login_invalid_password(monkeypatch):
 
     # Reset dependency overrides to avoid affecting other tests
     app.dependency_overrides = {}
-
